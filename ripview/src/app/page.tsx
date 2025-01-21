9522e7b95d45ad847ce72e027d314612a3e4ef1e
Edwintang2005 <91371446+Edwintang2005@users.noreply.github.com>
2025-01-21 20:41:17 +1100

    Added filtering functionality and switched to json

--- ripview/src/app/page.tsx
+++ ripview/src/app/page.tsx
@@ -4,9 +4,10 @@ import { useState } from "react";
 import styles from "./page.module.css";
 import Image from "next/image";
 import CsvHandler from "../components/CsvHandler";
+import StationJson from "./data/stationsInformation.json";
 
 function Header() {
-    return<div className={styles.header}>
+    return<div className={styles.navBar}>
         <Image
         className={styles.lightLogo}
         src="/favicon/favicon.svg"
@@ -21,94 +22,18 @@ function Header() {
 
 export default function Home() {
     const [csvData, setCsvData] = useState<string[][]>([]);
-
+    var records = StationJson.records;
+    records = records.filter((a) => /Train|Metro/.test((a[10] as string)));
     return (
         <div className={styles.page}>
             <main className={styles.main}>
                 <Header></Header>
-
-                <div className={styles.csvContainer}>
-
-                    {/* Title */}
-                    <h1 style={{ marginBottom: "20px", color: "var(--foreground)" }}>
-                        Transport Location Data
-                    </h1>
-
-                    {/* Handles the file upload
-                    When a file is loaded, send the data to setCsvData to store it */}
-                    <CsvHandler onDataLoaded={setCsvData} />
-
-                    {/* Display table IFF there is data
-                    && is like, if csvData has anything in it, then show the following */}
-                    {csvData.length > 0 && (
-                        // Set the overflow to auto so that the table can scroll{/*  */}
-                        <div style={{ overflowX: "auto" }}>
-                            <table
-                                style={{
-                                    borderCollapse: "collapse",
-                                    width: "100%",
-                                    marginTop: "20px",
-                                    color: "var(--foreground)",
-                                }}
-                            >
-                                {/* For each row, create a table row, 
-                                then for each piece of data, 
-                                create a table cell and add data and style it */}
-                                <tbody>
-                                    {csvData.map((row, index) => (
-                                        <tr key={index}>
-                                            {row.map((cell, cellIndex) => (
-                                                <td
-                                                    key={cellIndex}
-                                                    style={{
-                                                        border: "1px solid var(--border-color)",
-                                                        padding: "8px",
-                                                        color: "var(--foreground)",
-                                                    }}
-                                                >
-                                                    {cell}
-                                                </td>
-                                            ))}
-                                        </tr>
-                                    ))}
-                                </tbody>
-                            </table>
-                        </div>
-                    )}
-                </div>
-
-                <ol>
-                    <li>
-                        Get started by editing <code>src/app/page.tsx</code>.
-                    </li>
-                    <li>Save and see your changes instantly.</li>
-                </ol>
-
-                <div className={styles.ctas}>
-                    <a
-                        className={styles.primary}
-                        href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        <Image
-                            className={styles.logo}
-                            src="/vercel.svg"
-                            alt="Vercel logomark"
-                            width={20}
-                            height={20}
-                        />
-                        Deploy now
-                    </a>
-                    <a
-                        href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        className={styles.secondary}
-                    >
-                        Read our docs
-                    </a>
-                </div>
+                <h2>Station Names</h2>
+                <ul>
+                  {records.map((post) => (
+                    <li key={post[1]}>{'Name: ' + post[1] + ', TSN: ' + post[2]}</li>
+                  ))}
+                </ul>
             </main>
             <footer className={styles.footer}>
                 <a
