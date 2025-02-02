09641f123f7d727910ff01e3fc457e9c744f8879
Edwintang2005 <91371446+Edwintang2005@users.noreply.github.com>
2025-02-02 12:24:45 +1100

    Adding new map

--- ripview/src/app/mapInput/page.tsx
+++ ripview/src/app/mapInput/page.tsx
@@ -1,11 +1,12 @@
 'use client';
 import Image from "next/image";
+import styles from '../page.module.css';
 
 
 export default function Home() {
     return (
-        <div>
-            <Image src='/map/sydney-trains-network-map.svg' alt='network-map' useMap="#networkMap" width={0} height={0} style={{ width: '70%', height: 'auto' }}/>
+        <div className={styles.mapDiv}>
+            <Image src='/map/Sydney_Trains_Network_Map.svg' alt='network-map' useMap="#networkMap" width={0} height={0} style={{ width: '70%', height: 'auto' }}/>
             <map name='networkMap'></map>
         </div>
     );
