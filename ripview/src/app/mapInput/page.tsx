fafb0d9989f5a38a340a0ed13542002981035ca0
Edwintang2005 <91371446+Edwintang2005@users.noreply.github.com>
2025-02-02 15:35:17 +1100

    Installed @svgr/webpack

--- ripview/src/app/mapInput/page.tsx
+++ ripview/src/app/mapInput/page.tsx
@@ -1,13 +1,12 @@
 'use client';
-import Image from "next/image";
 import styles from '../page.module.css';
+import MySVG from '../../../public/map/Sydney_Trains_Network_Map.svg';
 
 
 export default function Home() {
     return (
         <div className={styles.mapDiv}>
-            <Image src='/map/Sydney_Trains_Network_Map.svg' alt='network-map' useMap="#networkMap" width={0} height={0} style={{ width: '70%', height: 'auto' }}/>
-            <map name='networkMap'></map>
+            <MySVG/>
         </div>
     );
 }
