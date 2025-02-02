2cc7dc83f302b74dbe6e91480e615ad9c896a7d8
Edwintang2005 <91371446+Edwintang2005@users.noreply.github.com>
2025-02-02 16:51:42 +1100

    Added extra groups in .svg map

--- ripview/src/app/mapInput/page.tsx
+++ ripview/src/app/mapInput/page.tsx
@@ -1,12 +1,17 @@
 'use client';
 import styles from '../page.module.css';
 import MySVG from '../../../public/map/Sydney_Trains_Network_Map.svg';
-
+import { useCallback } from 'react';
 
 export default function Home() {
+    const handleSVGClick = useCallback((event: { target: { id: any; }; }) => {
+        console.log("Function called");
+        const clickedId = event.target.id;
+        console.log('Clicked ID:', clickedId);
+    }, []);
     return (
         <div className={styles.mapDiv}>
-            <MySVG/>
+            <MySVG onClick={handleSVGClick} style={{ cursor: 'pointer' }}/>
         </div>
     );
 }
