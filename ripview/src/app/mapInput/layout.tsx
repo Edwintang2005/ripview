5efe14f2230b68000fb430c05d8416782e533b05
Edwintang2005 <91371446+Edwintang2005@users.noreply.github.com>
2025-02-05 22:29:45 +1100

    allowed map to be zoomable

--- ripview/src/app/mapInput/layout.tsx
+++ ripview/src/app/mapInput/layout.tsx
@@ -1,6 +1,14 @@
 import type { Metadata } from 'next';
 import '../globals.css';
 import { ReactNode } from 'react';
+import type { Viewport } from 'next';
+
+export const viewport: Viewport = {
+    width: 'device-width',
+    userScalable: true,
+    initialScale: 1,
+    viewportFit: "contain"
+};
 
 export const metadata: Metadata = {
     title: 'RipView',
